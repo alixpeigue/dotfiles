ac15fe826383b1dd9a0ab780a8de22dfae0286aa
Alix Peigue <irobot.moi@gmail.com>
2023-05-17 14:47:44 +0200

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,36 @@
+#powerline-daemon -q
+#set fish_function_path $fish_function_path "/usr/share/powerline/fish/"
+#powerline-setup
+
+#starship init fish | source
+
+#enable_transience
+
+#!/bin/bash
+
+# Key bindings
+
+#bind \cq 'lfcd'
+bind \cq 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
+alias vpninsa='sudo openconnect --user=apeigue@insa-lyon.fr --authgroup INSA --server sslvpn.cisr.fr'
+alias IF='cd ~/Documents/INSA/3IF'
+alias hx='helix'
+
+abbr --add gc git clone
+abbr --add gco git checkout
+abbr --add gca git commit -am
+abbr --add ga git add
+
+
+
+# alias cat='bat'
+
+set -Ux EDITOR helix
+set -Ux QT_QPA_PLATFORMTHEME qt5ct
+set -g fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+
+end
---
+++ scripts/displaymode/change.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# change.sh
+
+read -r a
+
+echo "$a"
+
+if [ "$a" == "duplicate" ]; then
+  hyprctl keyword monitor eDP-1,preferred,0x0,1
+  hyprctl keyword monitor HDMI-A-1,preferred,auto,1,mirror,eDP-1
+elif [ "$a" == "extend left" ]; then
+  hyprctl keyword monitor eDP-1,preferred,1920x0,1
+  hyprctl keyword monitor HDMI-A-1,preferred,0x0,1
+elif [ "$a" == "extend right" ]; then
+  hyprctl keyword monitor eDP-1,preferred,0x0,1
+  hyprctl keyword monitor HDMI-A-1,preferred,1920x0,1
+elif [ "$a" == "only main" ]; then
+  hyptctl keyword monitor eDP-1,preferred,0x0,1
+  hyprctl keyword monitor HDMI-A-1,disable
+elif [ "$a" == "only external" ]; then
+  hyprctl keyword monitor HDMI-A-1,preferred,0x0,1
+  hyprctl keyword monitor eDP-1,disable
+fi
+  
+  
+
